c3674043999d6ea5e7cbe302fda2fd5e8a6ad329
SB22SU <Alexandru.Adeaconitei@ing.com>
2023-07-31 10:58:20 +0300

    chore: refactor

--- app.js
+++ app.js
@@ -9,13 +9,27 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res
     .status(200)
     .json({ status: 'success', result: tours.length, data: { tours } });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const getTour = (req, res) => {
+  const tour = tours.find((tour) => tour.id === +req.params.id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'error',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: { tour: tours.find((tour) => tour.id === +req.params.id) },
+  });
+};
+
+const createTour = (req, res) => {
   const id = tours[tours.length - 1].id + 1;
   const tour = { id, ...req.body };
   console.log(tour);
@@ -27,7 +41,43 @@ app.post('/api/v1/tours', (req, res) => {
       res.status(201).json({ status: 'success', data: { tour } });
     }
   );
-});
+};
+
+const updateTour = (req, res) => {
+  if (!tours.find((tour) => tour.id === +req.params.id)) {
+    return res.status(404).json({
+      status: 'error',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<UPDATED TOUR>',
+    },
+  });
+};
+
+const deleteTour = (req, res) => {
+  if (!tours.find((tour) => tour.id === +req.params.id)) {
+    return res.status(404).json({
+      status: 'error',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+app.route('/api/v1/tours').get(getAllTours).get(getTour).post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
